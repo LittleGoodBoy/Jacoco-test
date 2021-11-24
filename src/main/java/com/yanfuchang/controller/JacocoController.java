00361d7cc420fb3d67e77973e45d4b711c461e74
严福昌 <yanfuchang@qike366.com>
2021-11-24 14:16:52 +0800

    1

--- src/main/java/com/yanfuchang/controller/JacocoController.java
+++ src/main/java/com/yanfuchang/controller/JacocoController.java
@@ -23,15 +23,4 @@ public class JacocoController {
         }
     }
 
-    @ResponseBody
-    @PostMapping(value = "/test2", consumes = "application/json")
-    public String jacoco2(@RequestBody Jacoco jacoco) {
-        if ("daling2".equals(jacoco.getData())) {
-            return "daling2";
-        } else if ("dalingjia2".equals(jacoco.getData())) {
-            return "dalingjia2";
-        } else {
-            return "222222";
-        }
-    }
 }
