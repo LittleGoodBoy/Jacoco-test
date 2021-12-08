0dcffb1978208930875a0245ad32ae2f7467f2ee
严福昌 <yanfuchang@qike366.com>
2021-12-08 11:17:30 +0800

    1

---
+++ src/main/java/com/yanfuchang/controller/Test.java
@@ -0,0 +1,14 @@
+package com.yanfuchang.controller;
+
+public class Test {
+
+    private static final String aa = "1";
+    
+    public static void main(String[] args) {
+
+        String[] s = "12345".split("_");
+        System.out.println(s.length);
+        System.out.println(s[0]);
+    }
+
+}
