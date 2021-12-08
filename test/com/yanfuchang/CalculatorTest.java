1ed190ff44d0aa2b6c17f63de685f83181bb55bd
严福昌 <yanfuchang@qike366.com>
2021-12-08 11:17:18 +0800

    1

---
+++ test/com/yanfuchang/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.yanfuchang;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        log.info("11111111111111");
+    }
+
+    @Test
+    public void testAdd1() {
+        log.info("22222222222222");
+    }
+}
